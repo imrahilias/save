9f6af6d1e330f7444217d12ff37cbf9e91e0d4d3
imrahilias <siegel.moritz@gmail.com>
2019-10-30 20:40:35 +0100

    sadoku ready for testing

--- 01-Ue2019-10-09/sadoku.f90
+++ 01-Ue2019-10-09/sadoku.f90
@@ -1,47 +1,36 @@
 ! moritz siegel 191016
 ! sudoku solver via simulated annealing & heat bath / metropolis
+! ij : 11 12 13 ...
+!      21 22 
+!      31    ...
+!      ...
 
-! QUESTIONS:
-! deviation from equilibral energy of 55*9*3 = 1215
-
+! OPEN ISSUES:
 
 program sadoku
   
   implicit none
   
-  integer, parameter :: imax=9, jmax=9, nn=8
-  integer, dimension(9), parameter :: im=[2,5,8,2,5,8,2,5,8] ! koords of middle items
-  integer, dimension(9), parameter :: jm=[2,2,2,5,5,5,8,8,8]
-  integer, dimension(9), parameter :: inn=[1,-1,0,0,1,1,-1,-1] ! nearest & next nearest neighbour coordinates
-  integer, dimension(9), parameter :: jnn=[0,0,1,-1,1,-1,1,-1]
+  logical, dimension(9,9) :: mask ! predefined sudoku values
+  integer, dimension(9), parameter :: im=[2,2,2,5,5,5,8,8,8] ! middlemen
+  integer, dimension(9), parameter :: jm=[2,5,8,2,5,8,2,5,8]
+  integer, dimension(9), parameter :: in=[-1,-1,-1,0,0,0,1,1,1] ! nearest neighbours
+  integer, dimension(9), parameter :: jn=[-1,0,1,-1,0,1,-1,0,1]
+  integer, dimension (9,9) :: sudoku, row, col, prt
+  integer :: i, j, m, n
+  integer :: flips, accept, energy
+  real*8 :: r, t
+  
 
-!  integer, dimension (imax,jmax) :: mr, seg, cor, err, erc, ers
-!  integer :: i,j,k,c, idummy,jdummy,inew,jnew, segnew
-!  integer :: flips, accept, errtot, errata(5), correct(5)
-!  real*8 :: r, t, enew,eold, perc, dist
-!  real*8, parameter :: jj=0.99d0, lambda=0.9d0, ti=10000d0, tf=0.001d0
-!  real*8, parameter :: far=0.20d0 ! coupling factor of the next nearest neighbours
-  logical, dimension(imax,jmax) :: mask
-  
-  ! init
-  t = ti
-  c = 1
-  flips = 0
-  err(:,:) = 0
-  erc(:,:) = 0
-  ers(:,:) = 0
-  errata(:) = 0
-  correct(:) = 0
-  accept = 0
-  
   call srand(time()) ! initialise random numbers
 
-  ! read sudoku, define mask (empty slots), random-seed those
-  open(unit=10, file='sudoku_in.dat')
-  do i = 1,imax
-     do j = 1,jmax
-        read() sudoku(i,j)
-        if ( sudoku_in(i,j) .eq. 0 ) then ! def mask := free slots
+  
+  ! read sudoku, define mask of empty slots, random-seed those
+  open(unit=10, file='sudoku.dat')
+  do i = 1,9
+     do j = 1,9
+        read(10) sudoku(i,j)
+        if ( sudoku(i,j) .eq. 0 ) then ! mask 1 := free slot
            mask(i,j) = .true.
            sudoku(i,j)  = int(9.d0*rand()) + 1
         else
@@ -49,96 +38,74 @@ program sadoku
         end if
      end do
   end do
-  close(unit=10)
-
-  eold = energy(sudoku)
-
-  ! divide & rule:  count errata
-  rows(:,:) = 0
-  cols(:,:) = 0
-  patch(:,:) = 0
-  do i = 1, imax
-     do j = 1, jmax
-        ! row sums
-        if ( rows(i, sudoku(i,j)) .eq. 0 ) then ! ask if number is still free
-           rows(i, sudoku(i,j)) = 1
-        else
-           errata(i,j) = errata(i,j) + 1 ! increment count if number is already taken
-        end if
-        
-        ! col sums
-        if ( cols(j, sudoku(i,j)) .eq. 0 ) then
-           cols(j, sudoku(i,j)) = 1
-        else
-           errata(i,j) = errata(i,j) + 1
-        end if
-
-        ! patch sums, using i & j as patch & next neighbours
-        imm = im(i)
-        jmm = jm(i)
-        inew = imm + inn(j)
-        jnew = jmm + jnn(j)
-        if ( patch(i, sudoku(inew,jnew)) .eq. 0 ) then
-           patch(i, sudoku(inew,jnew)) = 1
-        else
-           errata(i,j) = errata(i,j) + 1
-        end if
-     end do
-  end do
   
-  numbers 
-     colsum = sum(sudoku(m,:)
-
-     ! patch sums
-     patchsum = sudoku(i,j)
-     imm = im(m)
-     jmm = jm(m)
-     nn : do k = 1, nn ! loop over nearest neighbors
-        inew = i + inn(k)
-        jnew = j + jnn(k)
-        patchsum = patchsum + sudoku(inew,jnew)
-     end do nn
-     
-     ! change in energy
-     energy = abs(rowsum + colsum + patchsum - de)
-  end do divide
   
-              
+  ! divide & rule:  count energy
+  row(:,:) = -1 ! minus 1 := free slot
+  col(:,:) = -1
+  prt(:,:) = -1
+  patch : do m = 1, 9 ! select patch middlemen
+     element : do n = 1, 9 ! cycle through neighbours within selected patch
+        i = im(m) + in(n)
+        j = jm(m) + jn(n)
+        row(i,sudoku(i,j)) = row(i,sudoku(i,j)) + 1
+        col(j,sudoku(i,j)) = col(j,sudoku(i,j)) + 1
+        prt(m,sudoku(i,j)) = prt(m,sudoku(i,j)) + 1
+     end do element
+  end do patch
+  
+  
   ! sweeeeep
+  t = ti
+  c = 1
+  flips = 0
+  accept = 0
   open(unit=20, file='flips.dat')
-  cool : do while (t .gt. tf)
-     row : do i = 1, imax
-        col : do j = 1, jmax
+  open(unit=30, file='energy.dat')
+  cool : do while ( t .gt. tf )
+     patch : do m = 1, 9 ! select patch
+        element : do n = 1, 9 ! cycle through selected patch
+           i = im(m) + in(j)
+           j = jm(m) + jn(j)
            
-           if ( mask(i,j) .eqv. .false. ) break ! skip if the number is fixed parameter
+           if ( mask(i,j) .eqv. .false. ) break ! skip if current number is fixed parameter
            
-           ! flip pixel to random except old value
-           guess(i,j) = mod(sudoku(i,j) + int(8.d0*rand()) , 9) + 1
-                    
-           ! change in energy
-              energy = abs(energy - 3*sudoku(i,j) + + colsum + patchsum - de)
-              
-              ! check for floating point overflow & define r
-              if ( de/t .gt. 20.d0 ) then
-                 r = 0.d0
-              else if ( de/t .lt. 0.001d0 ) then
-                 r = 1.d0
-              else
-                 r = exp(-de/t)
-              end if
-              
-              ! accept or deny & metropolis algorithm
-              if ( rand() .lt. r ) then
-                 sudoku(i,j) = guess
-                 flips = flips + 1
-              end if
-           end do divide
-        end do col
-     end do row
+           ! flip field to random except old value
+           guess = mod(sudoku(i,j) + int(8.d0*rand()) , 9) + 1
+           
+           ! update rows & cols & patch & energy
+           drow = - abs(row(i,sudoku(i,j))) + abs(row(i,sudoku(i,j)) - 1) - abs(row(i,guess)) + abs(row(i,guess) + 1)
+           dcol = - abs(col(j,sudoku(i,j))) + abs(col(j,sudoku(i,j)) - 1) - abs(col(j,guess)) + abs(col(j,guess) + 1)
+           dprt = - abs(prt(m,sudoku(i,j))) + abs(prt(m,sudoku(i,j)) - 1) - abs(prt(m,guess)) + abs(prt(m,guess) + 1)
+           de = drow + dcol + dprt
+           
+           ! check for floating point overflow & define r
+           if ( de/t .gt. 20.d0 ) then
+              r = 0.d0
+           else if ( de/t .lt. 0.001d0 ) then
+              r = 1.d0
+           else
+              r = exp(-de/t)
+           end if
+           
+           ! accept or deny & metropolis algorithm
+           if ( rand() .lt. r ) then
+              sudoku(i,j) = guess
+              row(i,sudoku(i,j)) = row(i,sudoku(i,j)) - 1
+              row(i,guess) = row(i,guess) + 1
+              col(j,sudoku(i,j)) = col(j,sudoku(i,j)) - 1
+              col(j,guess) = col(j,guess) + 1
+              prt(m,sudoku(i,j)) = prt(m,sudoku(i,j)) - 1
+              prt(m,guess) = prt(m,guess) + 1
+              flips = flips + 1
+              energy = energy + de
+           end if
+        end do element
+     end do patch
      
      ! write & reset flips
-     write(*,*) 'run: ',c, 'temperature: ',t ,'flips: ',flips
-     write(20,*) c, t, flips
+     write(*,*) 'run: ',c, 'temperature: ',t ,'flips: ',flips, 'energy: ',energy
+     write(20,*) c, t, flips, energy
      accept = accept + flips
      flips = 0
      
@@ -147,19 +114,20 @@ program sadoku
      c = c + 1
      
   end do cool
-  close(unit=20)
+
   
   ! write image & compare
-  open(unit=30, file='sudoku_out.dat')
-  do i=1,imax
-     do j=1,jmax
-        write(30,'(3i5)') seg(i,j)
-        write(*,*) seg(i,j)
+  open(unit=40, file='sadoku.dat')
+  do i=1,9
+     do j=1,9
+        write(*,*) sudoku(i,j)
+        write(40,'(3i5)') sudoku(i,j)
      end do
      write(*,*)
   end do
-  close(unit=30)
+
   
-  write(*,*) 'runs: ',c, 'total flips: ',accept
+  write(*,*) 'runs: ',c, 'total flips: ',accept, 'final energy: ',energy
+
   
 end program sadoku
